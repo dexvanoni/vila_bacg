cc2c8c5cea5f12b16462da9e050bce2d32a5be15
Denis <dex.vanoni@gmail.com>
2023-03-12 12:22:12 -0300

    Editado arquivo de rota

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+$app_url = config("app.url");
+if (app()->environment('prod') && !empty($app_url)) {
+    URL::forceRootUrl($app_url);
+    $schema = explode(':', $app_url)[0];
+    URL::forceScheme($schema);
+};
 
 Route::get('/', function () {
     //return view('welcome');
