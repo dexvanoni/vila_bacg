cc93f91abb28c7cdea62dbd2700ecfac57de7d41
dexvanoni <dex.vanoni@gmail.com>
2024-04-16 20:02:45 -0400

    Novo ambiente de desenvolvimento

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">Faz o Login aqui! Testem GIT</div>
 
                 <div class="card-body">
 
